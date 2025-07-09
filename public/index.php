55592b148de8ef13a09b5b0e82dda59170b05756
mortezamatbou <mortezamatbou91@gmail.com>
2025-07-10 01:16:59 +0330

    remove files

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require_once('../caspian/config.php');
 require_once('../caspian/commons.php');
 
 require_once('../vendor/autoload.php');
-//require_once('../routes/api.php');
 
 $loader = new \Caspian\Loader();
 $loader->run();
