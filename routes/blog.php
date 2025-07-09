80d511d19512dff9632ae07115e5867e6331304d
mortezamatbou <mortezamatbou91@gmail.com>
2025-07-10 01:32:07 +0330

    add license

--- routes/blog.php
+++ routes/blog.php
@@ -5,4 +5,3 @@ use App\Controllers\blog\PostController;
 
 Router::get('/', [PostController::class, 'index']);
 Router::get('/posts', [PostController::class, 'posts']);
-//Router::post('midd/test', [Second::class, 'index'], ['Captcha.TestMiddleware' => []]);
